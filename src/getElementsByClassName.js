788c4f6e588372d502cf0d42d44be344ef1477ad
jimmylu1 <jimmy88lu@gmail.com>
2019-04-09 07:24:07 -0700

    Completed stringifyJSON and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,25 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var arr = [];
+
+
+  var recursiveSearch = function(node) {
+    // recursively call function if there are children
+    if (node.classList) {
+      if (node.classList.contains(className)) {
+        arr.push(node);
+      }
+    }
+    if (node.childNodes) {
+      for (let i = 0; i < node.childNodes.length; i++) {
+        recursiveSearch(node.childNodes[i]);
+      }
+    }
+  };
+  recursiveSearch(document.body);
+  return arr;
 };
+//
+
+// You should use document.body, element.childNodes, and element.classList
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,53 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+// Receives JS object
+// Need to convert to String
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // Identify object type
+  // If array
+  // Add string with '[' + val + ']'
+  // Recursively call stringifyJSON
+  // If object
+  // Add string with '{' + val + '}'
+  // Recursively call stringifyJSON
+  // Else
+  // Return value.toString()
+  var str = '';
+  var arr = [];
+
+  if (typeof obj !== 'object' || !obj) {
+    if (obj === null) {
+      return 'null';
+    } else if (typeof obj === 'string') {
+      return '"' + obj + '"';
+    } else {
+      return obj.toString();
+    }
+  }
+
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+
+    return '[' + arr.join(',') + ']';
+
+  } else if (typeof obj === 'object') {
+    if (Object.keys(obj).length === 0 && obj.constructor === Object) {
+      return '{}';
+    }
+
+    for (var key in obj) {
+      if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+        var value = stringifyJSON(obj[key]);
+        arr.push('"' + key + '"' + ':' + value);
+      }
+    }
+    return '{' + arr.join(',') + '}';
+  }
 };
